92d4ea83c5de63299331a103cd880abb91ab80e6
Ananiya Tesfahun <ananiyat24@gmail.com>
2024-10-16 21:23:23 +0300

    feat: define db and repo interfaces

---
+++ server/internal/domain/repositories.go
@@ -0,0 +1,57 @@
+package domain
+
+import (
+	"github.com/ananiyat/edit-wars/server/internal/domain/entities"
+
+	"github.com/google/uuid"
+)
+
+type OperationRepository interface {
+	FindOne(clientId uuid.UUID, counter int) (entities.Operation, error)
+	FindByClientId(clientId uuid.UUID) ([]entities.Operation, error)
+	FindByDocumentId(documentId uuid.UUID) ([]entities.Operation, error)
+	FindGeqCounter(clientId uuid.UUID, counter int) ([]entities.Operation, error)
+	FindAll() ([]entities.Operation, error)
+	Save(operation entities.Operation) error
+	SaveMany(operations []entities.Operation) error
+	Delete(operation entities.Operation) error
+	DeleteMany(operations []entities.Operation) error
+	Exists(id uuid.UUID) (bool, error)
+}
+
+type UserRepository interface {
+	Find(id uuid.UUID) (entities.User, error)
+	FindAll() ([]entities.User, error)
+	Save(user entities.User) error
+	Delete(user entities.User) error
+	Exists(id uuid.UUID) (bool, error)
+}
+
+type DocumentRepository interface {
+	Find(id uuid.UUID) (entities.Document, error)
+	FindAll() ([]entities.Document, error)
+	FindByUserId(userId uuid.UUID) ([]entities.Document, error)
+	Save(document entities.Document) error
+	Delete(document entities.Document) error
+	Exists(id uuid.UUID) (bool, error)
+}
+
+type CharacterRepository interface {
+	Find(id uuid.UUID) (entities.Character, error)
+	FindByDocumentId(documentId uuid.UUID) ([]entities.Character, error)
+	FindAll() ([]entities.Character, error)
+	Save(character entities.Character) error
+	SaveMany(characters []entities.Character) error
+	Delete(character entities.Character) error
+	DeleteMany(characters []entities.Character) error
+	Exists(id uuid.UUID) (bool, error)
+}
+
+type SessionRepository interface {
+	Find(id uuid.UUID) (entities.Session, error)
+	FindAll() ([]entities.Session, error)
+	FindByUserId(userId uuid.UUID) ([]entities.Session, error)
+	Save(session entities.Session) error
+	Delete(session entities.Session) error
+	Exists(id uuid.UUID) (bool, error)
+}
