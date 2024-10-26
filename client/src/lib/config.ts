8304d0ecbf8750a2c47ce3ab54286ebfc0d1c266
Ananiya Tesfahun <ananiyat24@gmail.com>
2024-10-26 23:24:30 +0300

    fix: fix vite env bug

--- client/src/lib/config.ts
+++ client/src/lib/config.ts
@@ -4,8 +4,8 @@ const DEFAULTS = {
     ssl: false
 }
 class Config {
-    static SERVER_BASE_URL = import.meta.env.SERVER_BASE_URL ?? DEFAULTS.baseUrl;
-    static SSL = import.meta.env.SSL === "true";
+    static SERVER_BASE_URL = import.meta.env.VITE_SERVER_BASE_URL ?? DEFAULTS.baseUrl;
+    static SSL = import.meta.env.VITE_SSL === "true";
 
     static formattedBaseUrl() {
         return `${this.SSL?"https":"http"}://${this.SERVER_BASE_URL}`
